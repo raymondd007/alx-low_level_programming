bd02d95d48040da390ac5d272c649ad53b324fb5
theroar! <naijaroar@email.com>
2022-12-12 18:43:02 -0800

    0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+   puts("\"programming is like buiding a multilingual puzzle");
+   return (0);
+
+}
